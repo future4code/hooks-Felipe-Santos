9227ffcab486783f1897e40032d5765562757ecf
ElreversMain <felipe123oliveirasantos@gmail.com>
2022-02-08 01:31:17 -0200

    Variaveis

---
+++ modulo1/Variaveis/index.js
@@ -0,0 +1,12 @@
+const nome1 = prompt("Digite um numero  ")
+const nome2 = prompt("digite outro numero ")
+const casa =Number(nome1)
+const house =Number(nome2)
+const x = casa+house
+console.log( nome1)
+console.log(nome2)
+console.log(x)
+const y= nome1*nome2
+console.log(y) 
+
+
