67f9d97cb00a982a8e7d9f8dd12b85f631cc8056
ElreversoMain <felipe123oliveirasantos@gmail.com>
2022-02-08 17:45:25 -0200

    Desafio

---
+++ modulo1/Variaveis/Desafio/index.js
@@ -0,0 +1,16 @@
+const nome1 = prompt("Digite um numero  ")
+const nome2 = prompt("digite outro numero ")
+const casa =Number(nome1)
+const house =Number(nome2)
+const x = casa+house
+console.log( nome1)
+console.log(nome2)
+console.log(x)
+const y= nome1*nome2
+console.log(y)
+
+// Voce escolhera um numero nome1 e nome2 
+// numero que vc escolhera aparecera ex 4 e 8
+// numero1 + numero 2 = x ex 4+8=12
+// valor de y sera numero1 * numero 2 = 36
+
---
+++ modulo1/Variaveis/Exercicio/index.js
@@ -0,0 +1,24 @@
+ let a =prompt ("Qual seu nome ? ")
+ let b = prompt("qual sua idade ? ")
+ console.log (typeof a , b )
+// string porque e um nome 
+// 22 porque e um numero 
+console.log (a , b )
+//  poderia ter colocabo B como Number  porem continua igual 
+
+
+// exercicio 2 
+let quest = prompt(" você esta usando blusa azul ?")
+let questt= prompt("voce esta usando calça rosa ?")
+let questtt=prompt(" voce esta usando chinelo roxo ? ")
+ let blusa = true
+ let calça = false
+ let chinelo=true
+ console.log( quest,questt,questtt, blusa, calça ,chinelo ) 
+
+
+ //Exercicio 3  
+ let aa=prompt ("escolha a sabendo que sera B ")
+ let bb=prompt(" escolha B sabendo que sera A ")
+ console.log( " O novo valor de A é ", b) // valor escolhido
+ console.log(" O novo valor de B sera ", a)// Valor escolhido
