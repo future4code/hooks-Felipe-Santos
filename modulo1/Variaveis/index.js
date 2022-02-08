1c108d5c005facea9928d9d3b110767408e144bd
ElreversMain <felipe123oliveirasantos@gmail.com>
2022-02-07 23:49:41 -0200

    Variaveis

---
+++ modulo1/Variaveis/index.js
@@ -0,0 +1,14 @@
+const nome1 = prompt("Digite um numero  ")
+const nome2 = prompt("digite outro numero ")
+console.log(nome1)
+console.log(nome2)
+const a=nome1
+const b=nome2
+const x= nome1-nome2
+console.log(x)
+const e=nome1
+const r=nome2
+const y= e*r
+console.log(y)
+
+
