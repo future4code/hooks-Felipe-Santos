74d2962b648495cd24d1e8fb74cd64ea645c26ff
ElreversoMain <felipe123oliveirasantos@gmail.com>
2022-02-10 12:17:58 -0200

    Operadores Exercicio/Desafio

---
+++ modulo1/Operadores/index.js
@@ -0,0 +1,48 @@
+
+//Exercicio Teorico 
+//Exercicio 01
+// 1 false 2 false  3 true  4 seria resultado false false true 
+//Exercicio 02 
+// soma sera em promp 12+12 1212
+// Exercicio03
+//let primeironumero=prompt ("Digite um numero ")
+//let segundonumero=prompt("Digite outro numero ")
+// let casa=primeironumero
+// let house=segundonumero
+// let casa=Number
+//let house=Number
+// const soma= casa+house
+// console.log(soma)
+
+
+// Exercicio01
+const nome =prompt(" qual e sua idade   ?")
+const nome2=prompt("qual e idade da sua colega ?")
+const nome3=prompt("Sua idade e maior que da sua colega ?")
+const numero =Number(nome)
+const numero2=Number(nome2)
+const diferença =nome>=nome2 
+console.log (diferença )
+
+// Exercicio02
+const nome =prompt("insira um numero par ")
+const numero=Number(nome)
+const dividir=numero/2
+console.log(dividir)
+
+//exercicio 03
+const nome=prompt("digite sua idade ")
+const meses = nome*12
+const dias =nome*365
+const horas=nome*8760
+console.log ("sua idade em ",meses,"meses",dias,"dias",horas ,"horas")
+
+// Exercicio 04
+const digite =prompt("digite um numero")
+const digite2=prompt("digite outro numero ")
+const maior = digite>digite2 
+const igual = digite==digite2
+
+console.log(maior)
+console.log(igual)
+
