2ffa78d02da875e134a56ef2516c577ecb7555f9
ElreversoMain <felipe123oliveirasantos@gmail.com>
2022-03-02 18:48:37 -0300

    Variaveis

---
+++ Modulo1/Variaveis/index.js
@@ -0,0 +1,26 @@
+// exercicio 01 - variaveis
+ const nome = prompt("digite um nome ")
+ const idadade=prompt("digite sua idade ")
+ console.log(`olá ${nome }, voce tem ${idadade} anos `)
+ // exercicio 02
+ 
+//let camisa
+let chinelo
+let roupa
+camisa=prompt("voce esta usando camisa ?")
+chinelo=prompt("voce esta usando chinelo ?")
+roupa=prompt("voce esta pelado ?")
+console.log(camisa,chinelo,roupa)
+
+//exercicio 03 
+let a=10
+let b=25
+console.log(`o valor a e b `,  b)
+console.log(`o valor de b e a `, a)
+//Desafio
+
+let insira=Number(prompt("insira um numero "))
+let insira2=Number(prompt("insira outro numero"))
+valorx=insira+insira2
+valory=insira*insira2
+console.log("valor de x é" ,valorx , "valor de y é" , valory ,)         
