1326a1f2bc5a1a94660e5238574751f7775155dc
ElreversoMain <felipe123oliveirasantos@gmail.com>
2022-03-08 22:50:49 -0300

    lacos

---
+++ Modulo1/lacos/index.js
@@ -0,0 +1,21 @@
+// exercicio escrita 
+// valor saira 10 porque i<5 e soma+=5 
+// exercicio 02 
+//saira valor 19 21 23 25 27 30 
+// exercicio 03
+// saira * e acrescentara * cada linha 
+
+const cachorro=Number(prompt("Quantos cachorros voce tem ?"))
+if (cachorro===1){  
+     console.log(cachorro)
+    let nome=prompt("digite nome do cachorro")
+    console.log(nome)
+    
+
+ } else if  (cachorro===0) {
+    console.log(`voce deveria adotar um `)
+ }else if (cachorro===2) {
+     let cachorro1=prompt("digite o nome")
+     let cachorro2=prompt("digite o nome")
+     console.log(cachorro1,cachorro2)
+ }
