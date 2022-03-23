2e333d082909ae488502a95dbb0c8482474fd494
ElreversoMain <felipe123oliveirasantos@gmail.com>
2022-03-22 21:45:01 -0300

    template-loja

---
+++ html-sistema-loja/Parte JS/index.js
@@ -0,0 +1,9 @@
+function calcularsalario(qtdeCarrosVendidos,valorTotalVenda){
+    const salariofixo= 2000
+    const quantidade=100*qtdeCarrosVendidos
+    const valortotal=valorTotalVenda*0.05
+    const total=salariofixo+quantidade+valortotal
+    return total
+
+
+}
