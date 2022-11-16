bffabe48cada13fdd3451fa2341b2c0581059452
ElreversoMAin <felipe123oliveirasantos@gmail.com>
2022-11-16 21:52:39 -0200

    jest-template

---
+++ BackEnd/jest-template/tests/ex1.test.ts
@@ -0,0 +1,41 @@
+//ex1
+test("Testing balance greater than value", () => {
+	const user: User = {
+		name: "Astrodev",
+		balance: 100
+	}
+
+	const result = performPurchase(user, 40)
+	
+	expect(result).toEqual({
+		name: "Astrodev",
+		balance: 60
+	})
+})
+
+//b
+test("Testing balance greater than value", () => {
+	const user: User = {
+		name: "Astrodev",
+		balance: 50
+	}
+
+	const result = performPurchase(user, 50)
+	
+	expect(result).toEqual({
+		...user,
+		balance: 0
+	})
+})
+
+//c 
+test("Testing balance greater than value", () => {
+	const user: User = {
+		name: "Astrodev",
+		balance: 30
+	}
+
+	const result = performPurchase(user, 50)
+	
+	expect(result).not.toBeDefined()
+})
